4c97ae6ac9deda3e56e52c70fe65fa540fbe541e
MigcalLan <John---Nash@outlook.com>
2023-05-12 08:33:31 +0800

    瀑布流网页解决

--- sportnew/sportnew/pipelines.py
+++ sportnew/sportnew/pipelines.py
@@ -10,12 +10,15 @@ from itemadapter import ItemAdapter
 
 class SportnewPipeline:
     def open_spider(self, spider):
+        print("爬虫开始")
         self.fp = open('news.json', 'w', encoding='utf-8')
 
     def process_item(self, item, spider):
+        print("爬虫进行中")
         self.fp.write(str(item))
         return item
 
     def close_spider(self, spider):
+        print("爬虫已完成")
         self.fp.close()
 
--- sportnew/sportnew/spiders/news.py
+++ sportnew/sportnew/spiders/news.py
@@ -1,66 +1,137 @@
 import time
-
 import scrapy
+from httpcore import TimeoutException
 from sportnew.items import News
 import unicodedata as ucd
 from selenium import webdriver
-from selenium.webdriver.edge.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from msedge.selenium_tools import EdgeOptions
+from msedge.selenium_tools import Edge
 
 
 class NewsSpider(scrapy.Spider):
     name = 'news'
     allowed_domains = ['sports.sina.com.cn']
     start_urls = ['https://sports.sina.com.cn']
-    ser = Service("..\\MicrosoftWebDriver.exe")
-    path = "MicrosoftWebDriver.exe"
-    browser = webdriver.Edge(service=ser)
+    edge_options = EdgeOptions()
+    edge_options.use_chromium = True
+    # 设置无界面模式，也可以添加其它设置
+    edge_options.add_argument('headless')
+    browser = Edge(options=edge_options)
 
     def parse(self, response, **kwargs):
         link_list = response.xpath("//ul[@class='links']//li/a")
         topic_list = link_list[4:11]
         for topic in topic_list:
             Topic = topic.xpath("./text()").extract_first()
-            print(Topic)
             url = topic.xpath("./@href").extract_first()
-            print(url)
             yield scrapy.Request(url=url, callback=self.parse_secound, meta={'Topic': Topic, 'url': url})
 
     def parse_secound(self, response):
         Topic = response.meta["Topic"]
         if Topic == "国际足球":
+            print(f"正在抓取{Topic}新闻")
             page_list = response.xpath("//ul[@class='ul-type1']//li/a")
             for page in page_list:
                 title = page.xpath("./text()").extract_first()
                 url = page.xpath("./@href").extract_first()
                 yield scrapy.Request(url=url, callback=self.parse_third, meta={'title': title, 'url': url,
                                                                                'Topic': Topic})
-        elif Topic == "中国足球":
-            surl = response.meta['url']
-            self.browser.get(surl)
-            # js_bottom = 'document.documentElement.scrollTop=100000'
-            # self.browser.execute_script(js_bottom)
-            # time.sleep(2)
-            pageList = self.browser.find_elements(by='xpath', value="//div[@class='feed-card-item']//h2/a")
-            self.browser.close()
-            for page in pageList:
-                title = page.text
-                print(title)
-                url = page.get_attribute('href')
-                print(url)
-                self.browser.get(url)
-                content = self.browser.find_elements(by='xpath', value="//div[@id='artibody']/p//text()")
-                self.browser.close()
-                news = News(Topic=Topic, title=title, content=content, url=url)
-                yield news
-
-
         elif Topic == "中国篮球":
+            print(f"正在抓取{Topic}新闻")
             page_list = response.xpath("//li[@class='item']/p/a")
             for page in page_list:
-                title = page.xpath("./text()").extract_first()
+                title = page.xpath("./text()").extract()
                 url = page.xpath("./@href").extract_first()
                 yield scrapy.Request(url=url, callback=self.parse_third, meta={'title': title, 'url': url,
                                                                                'Topic': Topic})
+        elif Topic == "综合":
+            page_list = response.xpath("//h2[@class='label']/a")
+            topic_list = page_list[2:11]
+            for topic in topic_list:
+                Topic = topic.xpath("./text()").extract_first()
+                url = topic.xpath("./@href").extract_first()
+                print(f"正在抓取{Topic}新闻")
+                yield scrapy.Request(url=url, callback=self.parse_four, meta={'url': url, 'Topic': Topic})
+
+        elif Topic == "中国足球":
+            print(f"正在抓取{Topic}新闻")
+            original_url = response.meta['url']
+            self.browser.get(original_url)
+            while True:
+                # 往下滚动
+                self.browser.execute_script("window.scrollBy(0, 50000);")
+                # 使用WebDriverWait等待页面加载完成
+                try:
+                    element_present = EC.presence_of_element_located((By.XPATH, "//div[@class='feed-card-item']//h2/a"))
+                    WebDriverWait(self.browser, 10).until(element_present)
+                except TimeoutException:
+                    break
+                # 从滚动加载的内容中查找链接
+                next_page = self.browser.find_elements_by_xpath("//span[@class='pagebox_next']/a")
+                if next_page:
+                    page_list = self.browser.find_elements_by_xpath("//div[@class='feed-card-item']//h2/a")
+                    break
+            for page in page_list:
+                title = page.text
+                url = page.get_attribute('href')
+                # 处理抓取到的链接
+                get_browser = Edge(options=self.edge_options)
+                get_browser.get(url)
+                content = []
+                content_list = get_browser.find_elements(by='xpath', value="//div[@id='artibody']/p")
+                if content_list == '':
+                    continue
+                else:
+                    for p in content_list:
+                        if p.text == '':
+                            continue
+                        else:
+                            content.append(p.text)
+                    get_browser.close()
+                    news = News(Topic=Topic, title=title, content=content, url=url)
+                    yield news
+
+        elif Topic == "NBA":
+            print(f"正在抓取{Topic}新闻")
+            original_url = response.meta['url']
+            self.browser.get(original_url)
+            while True:
+                # 往下滚动
+                self.browser.execute_script("window.scrollBy(0, 50000);")
+                # 使用WebDriverWait等待页面加载完成
+                try:
+                    element_present = EC.presence_of_element_located((By.XPATH, "//div[@class='feed-card-item']//h2/a"))
+                    WebDriverWait(self.browser, 10).until(element_present)
+                except TimeoutException:
+                    break
+                # 从滚动加载的内容中查找链接
+                next_page = self.browser.find_elements_by_xpath("//span[@class='pagebox_next']/a")
+                if next_page:
+                    page_list = self.browser.find_elements_by_xpath("//div[@class='feed-card-item']//h2/a")
+                    break
+            for page in page_list:
+                title = page.text
+                url = page.get_attribute('href')
+                # 处理抓取到的链接
+                get_browser = Edge(options=self.edge_options)
+                get_browser.get(url)
+                content = []
+                content_list = get_browser.find_elements(by='xpath', value="//div[@id='artibody']/p")
+                if content_list == '':
+                    continue
+                else:
+                    for p in content_list:
+                        if p.text == '':
+                            continue
+                        else:
+                            content.append(p.text)
+                    get_browser.close()
+                    news = News(Topic=Topic, title=title, content=content, url=url)
+                    yield news
 
     def parse_third(self, response):
 
@@ -78,9 +149,12 @@ class NewsSpider(scrapy.Spider):
         news = News(Topic=Topic, title=title, content=content_mlist, url=url)
         yield news
 
-    # def gertanotherway(self, url):
-    #
-    #     self.browser.get(url)
-    #     pageList = self.browser.find_elements(by='xpath', value="//div[@class='feed-card-item']/h2/a")
-    #     for page in pageList:
-    #         print(page.get_attribute('href'))
+    def parse_four(self, response):
+
+        page_list = response.xpath("//ul[@class='list2']/li/a")
+        Topic = response.meta['Topic']
+        for page in page_list:
+            title = page.xpath("./text()").extract_first()
+            url = page.xpath("./@href").extract_first()
+            yield scrapy.Request(url=url, callback=self.parse_third, meta={'title': title, 'url': url,
+                                                                           'Topic': Topic})
